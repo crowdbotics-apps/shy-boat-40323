5c12b6564b9dd013ed4df7041f1d0c083936d645
Slobodan Petrovic <slobodan.petrovic@crowdbotics.com>
2023-05-05 22:11:57 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "shy-boat-40323",
-  url: "https://shy-boat-40323.botics.co",
-  api: "https://shy-boat-40323.botics.co/api/v1"
+  "name": "shy-boat-40323",
+  "url": "https://shy-boat-40323.botics.co",
+  "api": "https://shy-boat-40323.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
